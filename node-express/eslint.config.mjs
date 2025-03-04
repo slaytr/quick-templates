b76adffa18673a5f73f2cc279d9e0fa21dcf6b5d
Bill Li <bill.li@siteminder.com>
2025-03-04 21:10:50 +1300

    add eslint

--- node-express/eslint.config.mjs
+++ node-express/eslint.config.mjs
@@ -1,12 +1,39 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+})
 
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+    ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+    {
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 2020,
+            sourceType: 'module',
+        },
+
+        rules: {
+            semi: ['error', 'never'],
+            quotes: ['error', 'single']
+        },
+    },
+]
